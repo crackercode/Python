b502dcf1b232510913630eaa46519be97c557051
cracker <crackerit92@gmail.com>
2022-05-27 13:21:31 +0530

    c

--- Input.py
+++ Input.py
@@ -1,2 +1,18 @@
-name = input("What is your name?")
-print("Hello " + name)
+# name = input("What is your name?")
+# print("Hello " + name)
+
+# birthYear = input("Enter your birth year : " )
+# age = 2022 - int(birthYear)
+# print(age)
+
+int()
+float()
+bool()
+str()
+
+firstNo = input("First Number : ")
+secondNo = input("Second Number: ")
+
+# sum  = firstNo + secondNo = 1020
+sum  = float(firstNo) + float(secondNo) 
+print("Sum " + str(sum))
